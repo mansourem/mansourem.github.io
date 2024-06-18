2330fa38a33a11ca4f944fda38a5cc4dd7b794d4
Elaine Mansour <mansour.elaine.1@gmail.com>
2024-06-18 11:47:30 -0400

    website draft init

---
+++ javascript/index.js
@@ -0,0 +1,88 @@
+var TxtType = function(el, toRotate, period) {
+   this.toRotate = toRotate;
+   this.el = el;
+   this.loopNum = 0;
+   this.period = parseInt(period, 10) || 2000;
+   this.txt = '';
+   this.tick();
+   this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+   var i = this.loopNum % this.toRotate.length;
+   var fullTxt = this.toRotate[i];
+
+   if (this.isDeleting) {
+   this.txt = fullTxt.substring(0, this.txt.length - 1);
+   } else {
+   this.txt = fullTxt.substring(0, this.txt.length + 1);
+   }
+
+   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+   var that = this;
+   var delta = 200 - Math.random() * 100;
+
+   if (this.isDeleting) { delta /= 2; }
+
+   if (!this.isDeleting && this.txt === fullTxt) {
+   delta = this.period;
+   this.isDeleting = true;
+   } else if (this.isDeleting && this.txt === '') {
+   this.isDeleting = false;
+   this.loopNum++;
+   delta = 500;
+   }
+
+   setTimeout(function() {
+   that.tick();
+   }, delta);
+};
+
+window.onload = function() {
+   var elements = document.getElementsByClassName('typewrite');
+   for (var i=0; i<elements.length; i++) {
+       var toRotate = elements[i].getAttribute('data-type');
+       var period = elements[i].getAttribute('data-period');
+       if (toRotate) {
+         new TxtType(elements[i], JSON.parse(toRotate), period);
+       }
+   }
+   // INJECT CSS
+   var css = document.createElement("style");
+   css.type = "text/css";
+   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+   document.body.appendChild(css);
+};
+
+var tlBtn = document.querySelector('.tl'),
+    tbl = document.getElementById('t');
+
+tlBtn.addEventListener('click', function() {
+  tbl.classList.toggle('fixed');
+}, false);
+
+var bg = $('#bg');
+$(window).scroll(function() {
+  var x = $(this).scrollTop();
+  bg.css('background-position', '0% ' + parseInt(-x / 10) + 'px');
+});
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
