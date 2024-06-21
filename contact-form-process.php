3d331155ebe66c8d6238d8273955c241f1215395
Elaine Mansour <mansouem.cin@gmail.com>
2024-06-21 13:59:43 -0400

    semicolons

--- contact-form-process.php
+++ contact-form-process.php
@@ -1,10 +1,10 @@
 <?php
 if($_POST["eessage"]) {
-    $subject = $_POST['subject']
-    $message = $_POST['message']
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
     $headers = 'From: info@website.com' . "\r\n" .
            'Reply-To: ' . $_POST['email']. "\r\n" .
            'X-Mailer: PHP/' . phpversion();
-    mail("mansour.elaine.1@gmail.com", $subject, $message $headers)
+    mail("mansour.elaine.1@gmail.com", $subject, $message $headers);
     die('Thank you for your email');
 }
